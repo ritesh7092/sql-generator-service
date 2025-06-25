3f9637ea9a153dd3c6998f6b9bfa76b1b19e5322
ritesh7092 <riteshrajtiwari.iiitbh@gmail.com>
2025-06-25 14:08:33 +0530

    Initialize main application with async support and performance optimizations.

--- src/main/java/com/Arth/sql_generator_service/SqlGeneratorServiceApplication.java
+++ src/main/java/com/Arth/sql_generator_service/SqlGeneratorServiceApplication.java
@@ -2,12 +2,22 @@ package com.Arth.sql_generator_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SqlGeneratorServiceApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SqlGeneratorServiceApplication.class, args);
-	}
+		// Optimize JVM settings for microservice
+		System.setProperty("spring.jmx.enabled", "false");
+		System.setProperty("spring.main.lazy-initialization", "true");
 
+		SpringApplication app = new SpringApplication(SqlGeneratorServiceApplication.class);
+
+		// Performance optimizations
+		app.setLazyInitialization(true);
+		app.setRegisterShutdownHook(true);
+
+		app.run(args);
+	}
 }
