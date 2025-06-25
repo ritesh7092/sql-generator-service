0a98b821ba7c5eb43e7b253ceba8e855f5a1a54a
ritesh7092 <riteshrajtiwari.iiitbh@gmail.com>
2025-06-25 14:23:51 +0530

    Web configuration added

---
+++ src/main/java/com/Arth/sql_generator_service/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.Arth.sql_generator_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:300")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
